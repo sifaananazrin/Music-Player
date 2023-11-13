321d6f5ee51ea0f4b882934e654a6e734a78b965
Shifana nasrin <shifananazrin15@gmail.com>
2023-11-13 13:33:18 +0530

    Updated container configurations: port reallocation

--- src/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://ws.audioscrobbler.com",
-});
-
-export default instance;
--- src/api/request.js
+++
@@ -1,7 +0,0 @@
-const requests = {
-    getAlbums:
-      "/2.0/?method=artist.gettopalbums&artist=cher&api_key=9b860687df8e193128268b4f60adcda3&format=json",
-  };
-  
-  export default requests;
-  
